3bf331e50d8765c5e22c02c1a4e5d2b0c9da8134
Patrick Dillon <pdd@redhat.com>
2018-10-02 14:05:51 -0400

    Add new script to create directories and git worktree.

---
+++ new-project.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+name=$1
+branch=$2
+commitish=$3
+
+if [ -z $name ]; then
+    echo "Provide a name for the dir & git branch."
+    exit 2
+fi
+
+mkdir $name
+cd openshift-ansible
+git worktree prune
+
+if [ -z $commitish ] && [ -z $branch ]; then
+    
+    git pull upstream master
+    git worktree add -b $name ../$name/openshift-ansible
+    
+elif [ -z $commitish ]; then
+    echo "Fetch."
+    #git fetch upstream $branch:$name
+    echo "Add worktree."
+    git worktree add -B $name ../$name/openshift-ansible upstream/$branch 
+
+	 
+else
+    
+    git checkout $branch
+    git pull upstream $commitish:$name
+    git worktree add -B $name ../$name/openshift-ansible
+    
+fi
+
+cd ..
+(cp -r aws-template/* $name)
